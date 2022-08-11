9810612c3d9fd365b0a30e39ff6144a91c8ad7ab
Ken Chen <ken.chen@cargosmart.com>
2022-08-11 13:39:13 +0800

    Replace request to axios

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require('requestretry');
+const axios = require('axios').default;
 
 module.exports = function (session) {
   const Store = session.Store;
@@ -11,49 +11,66 @@ module.exports = function (session) {
         throw new Error('Missing uri option');
       }
 
-      this.request = request.defaults({
-        baseUrl: options.uri,
-        json: true,
+      this.request = axios.create({
+        baseURL: options.uri,
         timeout: options.timeout || 6000,
-        headers: {'user-agent': 'connect-remote'}
+        headers: { 'user-agent': 'connect-remote', 'accept': 'application/json' }
       });
     }
 
     // Required
     destroy(sid, cb) {
-      this.request.del(`/${sid}`, cb);
+      this.request.delete(`/${sid}`)
+        .then((response) => {
+          cb(null, response.data.data)
+        })
+        .catch(cb);
     }
     get(sid, cb) {
-      this.request.get(`/${sid}`, (err, response, body) => {
-        cb(err, body ? body.data : null);
-      });
+      this.request.get(`/${sid}`)
+        .then((response) => {
+          cb(null, response.data.data)
+        })
+        .catch(cb);
     }
     set(sid, session, cb) {
-      this.request.put(`/${sid}`, { body: session }, (err, response, body) => {
-        cb(err, body ? body.data : null);
-      });
+      this.request.put(`/${sid}`, session)
+        .then((response) => {
+          cb(null, response.data.data)
+        })
+        .catch(cb);
     }
 
     // Optional
     all(cb) {
-      this.request.get('/', (err, response, body) => {
-        cb(err, body ? body.data : null);
-      });
+      this.request.get('/')
+        .then((response) => {
+          cb(null, response.data.data)
+        })
+        .catch(cb);
     }
     clear(cb) {
-      this.request.del('/', cb);
+      this.request.delete('/')
+        .then((response) => {
+          cb(null, response.data.data)
+        })
+        .catch(cb);
     }
     length(cb) {
-      this.request.get('/count', (err, response, body) => {
-        const len = parseInt(body ? body.data : null);
-        cb(err, len);
-      });
+      this.request.get('/count')
+        .then((response) => {
+          const len = parseInt(response.data.data);
+          cb(null, len)
+        })
+        .catch(cb);
     }
     // Recommended
     touch(sid, session, cb) {
-      this.request.post(`/${sid}`, { body: session }, (err, response, body) => {
-        cb(err, body ? body.data : null);
-      });
+      this.request.post(`/${sid}`, session)
+        .then((response) => {
+          cb(null, response.data.data)
+        })
+        .catch(cb);
     }
   }
 
